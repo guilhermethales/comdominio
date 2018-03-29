3b9d3381cb0f8c639f9239d6e5820da3ce8f73c0
Guilherme Thales <guilherme.simoes@alpes.one>
2018-03-29 13:02:42 -0300

    fix global api url

--- src/http.js
+++ src/http.js
@@ -2,7 +2,7 @@ import { defaults } from 'lodash'
 import axios from 'axios'
 
 export const createClient = (options = {}) => axios.create(defaults({
-  baseURL: 'http://34.238.67.140/app/',
+  baseURL: 'http://www.comdomino.com/app/',
   headers: {'X-Custom-Header': 'foobar'}
 }, options))
 
